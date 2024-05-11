796389e4cdb93c03ac0d48232dbb8b407707e8c2
EzequielGuerrero <ezequiel_guerrero1@hotmail.com>
2024-05-11 15:09:47 +0200

    Modifico menu consola

--- src/main/java/DesafioLogica8/MenuGestion.java
+++ src/main/java/DesafioLogica8/MenuGestion.java
@@ -5,119 +5,111 @@ import java.util.Scanner;
 
 public class MenuGestion {
 
-	public MenuGestion() {
+	private boolean respuesta = true;
 
+	public MenuGestion() {
 		iniciarMenu();
 	}
 
 	private void iniciarMenu() {
-
 		Scanner scn = new Scanner(System.in);
-		Scanner opcMenu = new Scanner(System.in);
 		Conexion con = new Conexion();
 
-		System.out.println("Bienvenido al gestor, elige una de las siguientes opciones:");
-		System.out.println(" 1. Cines." + "\n 2. Peliculas." + "\n 3. Salas.");
+		while (respuesta) {
+			System.out.println("Bienvenido al gestor, elige una de las siguientes opciones:");
+			System.out.println(" 1. Cines." + "\n 2. Peliculas." + "\n 3. Salas." + "\n 4. Salir.");
 
-		int opcion = scn.nextInt();
+			int opcion = scn.nextInt();
+			scn.nextLine();
 
-		switch (opcion) {
+			switch (opcion) {
+			case 1:
+				System.out.println("¿Que operacion quieres realizar de las siguientes?:");
+				System.out.println(" 1.Crear registro." + "\n 2.Modificar un registro" + "\n 3.Borrar un registro "
+						+ "\n 4.Visializar todos los registros de la tabla ");
 
-		case 1:
+				int opcionInterna = scn.nextInt();
+				scn.nextLine();
 
-			System.out.println("¿Que operacion quieres realizar de las siguientes:?");
-			System.out.println(" 1.Crear registro." + "\n 2.Modificar un registro" + "\n 3.Borrar un registro "
-					+ "\n 4.Visializar todos los registros de la tabla ");
+				if (opcionInterna == 1) {
+					insertCines(scn, con);
+				}
+				if (opcionInterna == 2) {
+					menuModificacion(scn, con);
+				}
+				if (opcionInterna == 3) {
+					menuDeleteCines(scn, con);
+				}
+				if (opcionInterna == 4) {
+					con.leerCines(con.getConection());
+				}
+				break;
 
-			int opcionInterna = scn.nextInt();
+			case 2:
+				System.out.println("¿Que operacion quieres realizar de las siguientes?:");
+				System.out.println(" 1.Crear registro." + "\n 2.Modificar un registro" + "\n 3.Borrar un registro "
+						+ "\n 4.Visializar todos los registros de la tabla ");
 
-			if (opcionInterna == 1) {
-				insertCines(opcMenu, con);
+				int menuDos = scn.nextInt();
+				scn.nextLine();
+
+				if (menuDos == 1) {
+					insertPelis(scn, con);
+				}
+				if (menuDos == 2) {
+					modificarPelis(scn, con);
+				}
+				if (menuDos == 3) {
+					eliminarPeli(scn, con);
+				}
+				if (menuDos == 4) {
+					con.leerPeliculas(con.getConection());
+				}
+				break;
+
+			case 3:
+				System.out.println("¿Que operacion quieres realizar de las siguientes?:");
+				System.out.println(" 1.Crear registro." + "\n 2.Modificar un registro" + "\n 3.Borrar un registro "
+						+ "\n 4.Visializar todos los registros de la tabla ");
+
+				int menuTres = scn.nextInt();
+				scn.nextLine();
+
+				if (menuTres == 1) {
+					insertSalas(scn, con);
+				}
+				if (menuTres == 2) {
+					actualizarSalas(scn, con);
+				}
+				if (menuTres == 3) {
+					borrarSala(scn, con);
+				}
+				if (menuTres == 4) {
+					con.verSalas(con.getConection());
+				}
+				break;
+
+			default:
+
+				if (opcion > 3) {
+					respuesta = false;
+					scn.close();
+					System.out.println("Hasta Luego.");
+
+				}
 			}
-			if (opcionInterna == 2) {
-				menuModificacion(opcMenu, con);
-			}
-			if (opcionInterna == 3) {
-				menuDeleteCines(opcMenu, con);
-			}
-			if (opcionInterna == 4) {
-
-				con.leerCines(con.getConection());
-			}
-
-			break;
-
-		case 2:
-
-			System.out.println("¿Que operacion quieres realizar de las siguientes:?");
-			System.out.println(" 1.Crear registro." + "\n 2.Modificar un registro" + "\n 3.Borrar un registro "
-					+ "\n 4.Visializar todos los registros de la tabla ");
-
-			int menuDos = scn.nextInt();
-
-			if (menuDos == 1) {
-
-				insertPelis(opcMenu, con);
-			}
-			if (menuDos == 2) {
-
-				modificarPelis(opcMenu, con);
-			}
-			if (menuDos == 3) {
-
-				eliminarPeli(opcMenu, con);
-			}
-			if (menuDos == 4) {
-
-				con.leerPeliculas(con.getConection());
-			}
-
-			break;
-
-		case 3:
-
-			System.out.println("¿Que operacion quieres realizar de las siguientes:?");
-			System.out.println(" 1.Crear registro." + "\n 2.Modificar un registro" + "\n 3.Borrar un registro "
-					+ "\n 4.Visializar todos los registros de la tabla ");
-
-			int menuTres = scn.nextInt();
-
-			if (menuTres == 1) {
-
-				insertSalas(opcMenu, con);
-
-			}
-			if (menuTres == 2) {
-
-				actualizarSalas(opcMenu, con);
-			}
-			if (menuTres == 3) {
-
-				borrarSala(opcMenu, con);
-			}
-			if (menuTres == 4) {
-				con.verSalas(con.getConection());
-			}
-			break;
-
-		default:
-
-			System.out.println("Opcion no valida");
-			scn.close();
-			opcMenu.close();
 		}
 	}
 
-	public static void actualizarSalas(Scanner opcMenu, Conexion con) {
+	public static void actualizarSalas(Scanner scn, Conexion con) {
 		try {
-
 			System.out.println("Ingresa el indice de la sala a modificar:");
-
-			int indiceMod = opcMenu.nextInt();
+			int indiceMod = scn.nextInt();
+			scn.nextLine();
 
 			System.out.println("Ingresa el nuevo tamaño de la sala:");
-
-			int nuevoTamaño = opcMenu.nextInt();
+			int nuevoTamaño = scn.nextInt();
+			scn.nextLine();
 
 			con.modificarSalas(con.getConection(), nuevoTamaño, indiceMod);
 
@@ -126,11 +118,12 @@ public class MenuGestion {
 		}
 	}
 
-	public static void borrarSala(Scanner opcMenu, Conexion con) {
+	public static void borrarSala(Scanner scn, Conexion con) {
 		try {
-
 			System.out.println("¿Que sala quieres eliminar?");
-			int salaDelete = opcMenu.nextInt();
+			int salaDelete = scn.nextInt();
+			scn.nextLine();
+
 			con.eliminarSalas(con.getConection(), salaDelete);
 
 		} catch (SQLException e) {
@@ -138,29 +131,26 @@ public class MenuGestion {
 		}
 	}
 
-	public static void modificarPelis(Scanner opcMenu, Conexion con) {
+	public static void modificarPelis(Scanner scn, Conexion con) {
 		try {
-
 			System.out.println("Introduce el indice de la pelicula a la que deseas cambiar el nombre:");
-			int indiceMoficiado = opcMenu.nextInt();
-			opcMenu.nextLine();
+			int indiceMoficiado = scn.nextInt();
+			scn.nextLine();
+
 			System.out.println("Introduce el nuevo nombre:");
-			String nuevoNombre = opcMenu.nextLine();
+			String nuevoNombre = scn.nextLine();
 
 			con.modificarNombrePeli(con.getConection(), indiceMoficiado, nuevoNombre);
-
 		} catch (SQLException e) {
 			e.printStackTrace();
-
 		}
 	}
 
-	public static void eliminarPeli(Scanner opcMenu, Conexion con) {
-
+	public static void eliminarPeli(Scanner scn, Conexion con) {
 		try {
 			System.out.println("Introduce el indice de la pelicula a eliminar:");
-
-			int indiceDell = opcMenu.nextInt();
+			int indiceDell = scn.nextInt();
+			scn.nextLine();
 
 			con.eliminarPeliculas(con.getConection(), indiceDell);
 
@@ -169,11 +159,11 @@ public class MenuGestion {
 		}
 	}
 
-	public static void menuDeleteCines(Scanner opcMenu, Conexion con) {
+	public static void menuDeleteCines(Scanner scn, Conexion con) {
 		try {
-
 			System.out.println("¿Que numero de cine quieres eliminar?");
-			int cineEliminado = opcMenu.nextInt();
+			int cineEliminado = scn.nextInt();
+			scn.nextLine();
 
 			con.eliminarCines(con.getConection(), cineEliminado);
 
@@ -182,84 +172,81 @@ public class MenuGestion {
 		}
 	}
 
-	public static void menuModificacion(Scanner opcMenu, Conexion con) {
+	public static void menuModificacion(Scanner scn, Conexion con) {
 		try {
-
 			System.out.println("Introduce el indice del cine al que deseas cambiar el nombre:");
-			int indiceMoficiado = opcMenu.nextInt();
-			opcMenu.nextLine();
+			int indiceMoficiado = scn.nextInt();
+			scn.nextLine(); 
+
 			System.out.println("Introduce el nuevo nombre:");
-			String nuevoNombre = opcMenu.nextLine();
+			String nuevoNombre = scn.nextLine();
 
 			con.modificarNombreCine(con.getConection(), indiceMoficiado, nuevoNombre);
-
 		} catch (SQLException e) {
-
 			e.printStackTrace();
 		}
 	}
 
-	public static void insertCines(Scanner opcMenu, Conexion con) {
+	public static void insertCines(Scanner scn, Conexion con) {
 		try {
 			System.out.println("Introduce el nombre del cine que quieres introducir:");
-			String nombreCine = opcMenu.nextLine();
+			String nombre = scn.nextLine();
 
 			System.out.println("Introduce la dirrecion del cine:");
-			String direccion = opcMenu.nextLine();
+			String direccion = scn.nextLine();
 
-			con.crearCines(con.getConection(), nombreCine, direccion);
+			con.crearCines(con.getConection(), nombre, direccion);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static void insertSalas(Scanner opcMenu, Conexion con) {
+	public static void insertSalas(Scanner scn, Conexion con) {
 		try {
 			System.out.println("Introduce las caracteristicas de las salas de cine:");
-
 			System.out.println("¿Que capacidad de personas puede contener?");
-			int capacidad = opcMenu.nextInt();
+			int capacidad = scn.nextInt();
+			scn.nextLine();
 
 			System.out.println("¿Y que superficie tiene la sala?");
-			double superficie = opcMenu.nextDouble();
+			double superficie = scn.nextDouble();
+			scn.nextLine();
 
 			con.crearSalas(con.getConection(), capacidad, superficie);
-
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("La sala no ha podido ser guardada");
 		}
 	}
 
-	public static void insertPelis(Scanner opcMenu, Conexion con) {
+	public static void insertPelis(Scanner scn, Conexion con) {
 		try {
 			System.out.println("Introduce los datos de la pelicula a introducir:");
-
 			System.out.println("Titulo de la Pelicula:");
-
-			String titulo = opcMenu.nextLine();
+			String titulo = scn.nextLine();
 
 			System.out.println("Genero:");
-			String genero = opcMenu.nextLine();
+			String genero = scn.nextLine();
 
 			System.out.println("Director:");
-			String director = opcMenu.nextLine();
+			String director = scn.nextLine();
 
 			System.out.println("Duracion de la pelicula:");
-			int duracion = opcMenu.nextInt();
+			int duracion = scn.nextInt();
+			scn.nextLine();
 
 			System.out.println("Clasificacion de edad:");
-			int clasificacionEdad = opcMenu.nextInt();
+			int clasificacionEdad = scn.nextInt();
+			scn.nextLine();
 
 			System.out.println("Precio:");
-			int precio = opcMenu.nextInt();
+			int precio = scn.nextInt();
+			scn.nextLine();
 
 			con.crearPeliculas(con.getConection(), titulo, duracion, genero, director, clasificacionEdad, precio);
-
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println("La pelicula no se ha podido introducir en la BD");
 		}
 	}
-
 }
